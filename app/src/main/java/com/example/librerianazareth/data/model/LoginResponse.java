e8dfbdbd37e945f028061b70348c76dd1053eeab
Jesica-A <jess.ara95@outlook.com>
2026-09-25 18:00:28 -0300

    config: agregar modelo LoginResponse

---
+++ app/src/main/java/com/example/librerianazareth/data/model/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.librerianazareth.data.model;
+
+public class LoginResponse {
+    private String access;
+    private String refresh;
+
+    public String getAccess() {
+        return access;
+    }
+
+    public String getRefresh() {
+        return refresh;
+    }
+}
